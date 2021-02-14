3d57573102cae94acda50fe4f8b386a1821231ac
heriksuseno <>
2021-02-14 17:39:30 +0700

    Tugas 9 JS untuk NIOMIC

---
+++ tugas9.js
@@ -0,0 +1,12 @@
+function datadiri () {
+  var biodata = {
+    nama : 'Herik Suseno',
+    tempat : 'Kuningan',
+    tgl :'21 Januari 1991',
+    agama : 'Islam',
+  }
+  for(var x in biodata){
+    console.log(biodata[x])
+  }
+}
+datadiri()
